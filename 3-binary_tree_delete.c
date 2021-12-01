9810f5e80b052263ad9edd4af0a79baf2e8a7cad
khouzi <khouzem.boussaid@gmail.com>
2021-12-01 12:41:42 +0100

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,14 +6,11 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
-    else
-    {
-        if (tree->left != NULL)
-            binary_tree_delete(tree->left);
-        if (tree->right != NULL)
-            binary_tree_delete(tree->right);
-    }
-    free(tree);
+	if (tree == NULL)
+		return;
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
+	free(tree);
 }
