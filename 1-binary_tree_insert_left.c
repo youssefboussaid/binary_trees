a6ddf7cedd096263a91488c0dc4e2084dfab8227
khouzi <khouzem.boussaid@gmail.com>
2021-12-01 12:39:36 +0100

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,18 +11,18 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *left_node;
+	binary_tree_t *left_node;
 
-    if (parent == NULL)
-        return NULL;
-    left_node = binary_tree_node(parent, value);
-    if (left_node == NULL)
-        return NULL;
-    if (parent->left != NULL)
-    {
-        parent->left->parent = left_node;
-        left_node->left = parent->left;
-    }
-    parent->left = left_node;
-    return left_node;
+	if (parent == NULL)
+		return (NULL);
+	left_node = binary_tree_node(parent, value);
+	if (left_node == NULL)
+		return (NULL);
+	if (parent->left != NULL)
+	{
+		parent->left->parent = left_node;
+		left_node->left = parent->left;
+	}
+	parent->left = left_node;
+	return (left_node);
 }
