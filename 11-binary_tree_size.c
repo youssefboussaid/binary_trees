bf9a9669695a4bfe77f223ab78a3c2dce851bfc7
khouzi <khouzem.boussaid@gmail.com>
2021-12-01 16:01:29 +0100

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,17 +6,14 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t size = 0;
+	size_t size = 0;
 
-    if (tree == NULL)
-        return 0;
-    else
-    {
-        size += 1;
-        if (tree->left)
-            size += binary_tree_size(tree->left);
-        if (tree->right)
-            size += binary_tree_size(tree->right);
-        return size;
-    }
+	if (tree == NULL)
+		return (0);
+	size += 1;
+	if (tree->left)
+		size += binary_tree_size(tree->left);
+	if (tree->right)
+		size += binary_tree_size(tree->right);
+	return (size);
 }
