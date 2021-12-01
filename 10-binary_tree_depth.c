882a27d9259fb5038bee37f21b937eb862d042d5
khouzi <khouzem.boussaid@gmail.com>
2021-12-01 16:11:20 +0100

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,13 +7,13 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t dpt = 0;
+	size_t dpt = 0;
 
-    if (tree == NULL)
-        return(0);
-    if (tree->parent == NULL)
-        return (0);
-    dpt = binary_tree_depth(tree->parent);
-    dpt ++;
-    return (dpt);
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
+		return (0);
+	dpt = binary_tree_depth(tree->parent);
+	dpt++;
+	return (dpt);
 }
