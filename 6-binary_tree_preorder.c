6b8ab191a73116d8858b543618ded666400fa41d
khouzi <khouzem.boussaid@gmail.com>
2021-12-01 12:45:20 +0100

    fix

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,14 +7,11 @@
  * The value in the node must be passed as a parameter to this function.
  * Return: Nothing
  */
- void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
-    else
-    {
-        func(tree->n);
-        binary_tree_preorder(tree->left, func);
-        binary_tree_preorder(tree->right, func);
-    }
+	if (tree == NULL || func == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
