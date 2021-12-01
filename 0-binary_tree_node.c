000f9d4623ab95d2773944af4e057c42ffa4d670
khouzi <khouzem.boussaid@gmail.com>
2021-12-01 12:36:08 +0100

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,13 +9,13 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *tmp = malloc(sizeof(binary_tree_t));
+	binary_tree_t *tmp = malloc(sizeof(binary_tree_t));
 
-    if (tmp == NULL)
-        return (NULL);
-    tmp->parent = parent;
-    tmp->left = NULL;
-    tmp->right = NULL;
-    tmp-> n = value;
-    return (tmp);
+	if (tmp == NULL)
+		return (NULL);
+	tmp->parent = parent;
+	tmp->left = NULL;
+	tmp->right = NULL;
+	tmp-> n = value;
+	return (tmp);
 }
