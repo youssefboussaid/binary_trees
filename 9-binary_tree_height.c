6282e8771cfdb605fffd8458abc5a0cac69cee37
youssefboussaid <youssbsd@gmail.com>
2021-12-01 15:16:53 -1100

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,16 +7,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int left_h, right_h = 0;
+	int left_h, right_h = 0;
 
-    if (tree == NULL)
-        return (0);
-    else
-    {
-        left_h = binary_tree_height(tree->left);
-        right_h = binary_tree_height(tree->right);
-        if (left_h >= right_h)
-            return(left_h + 1);
-        return (right_h + 1);
-    }
+	if (tree == NULL)
+		return (0);
+	if (tree->right == NULL && tree->left == NULL)
+		return (0);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+	if (left_h >= right_h)
+		return(left_h + 1);
+	return (right_h + 1);
 }
