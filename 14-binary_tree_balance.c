8617af5c95b9211d996697592669fb1fae432881
khouzi <khouzem.boussaid@gmail.com>
2021-12-02 03:15:26 +0100

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,17 +12,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->right == NULL && tree->left == NULL)
-		return (0);
 	left_h = binary_tree_height(tree->left);
 	right_h = binary_tree_height(tree->right);
 	if (left_h >= right_h)
 		return (left_h + 1);
 	return (right_h + 1);
 }
-
-
-
 /**
  * binary_tree_balance - function that measures the balance
  * factor of a binary tree.
@@ -31,13 +26,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left, right = 0;
-    
-    if (tree == NULL)
-        return (0);
-    if (tree->right)
-        right = binary_tree_height(tree->right);
-    if (tree->left)
-        left = binary_tree_height(tree->left);
-    return (left - right);
+	int left, right = 0;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (left - right);
 }
